00a2baf270d5c86c4527ca803bd9d890108cfb01
Owen Crucefix <OwenCrucefix@users.noreply.github.com>
2017-05-19 13:32:30 +0100

    Add files via upload

---
+++ Python Text based RPG.py
@@ -0,0 +1,8 @@
+from random import randint
+from time import sleep
+import sys
+
+inventory = {"Weapon":"Wooden Sword",
+             "Armour":"None",
+             "Backpack":{"slot1":"Potion","slot2":"Potion","slot3":"Empty"},
+             "Gold":0}
